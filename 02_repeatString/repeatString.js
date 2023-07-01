6c4929faed5ed3148e1489bbe72368477990f455
Vince Ivan Minez <banongofficial28@gmail.com>
2023-07-01 17:07:30 +0800

    Completed Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    myStr = ""
+    if (num > 0 && str !== "") {
+        for (let i = 0; i < num; i++) {
+            myStr += str;
+        }
+    } else if (num < 0  && str !== "") {
+        myStr = "ERROR";
+    }
+    
 
+
+    return myStr;
 };
 
 // Do not edit below this line
