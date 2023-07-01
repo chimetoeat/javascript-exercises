18e7f912b153fe761458adf1b2e9d3f732bf72f8
Vince Ivan Minez <banongofficial28@gmail.com>
2023-07-01 17:15:03 +0800

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    myStr = ""
+    if (str !== "") {
+        for (let i = str.length-1; i >= 0; i--) {
+            myStr += str[i]
+        }
+    }
+    
 
+    return myStr;
 };
 
 // Do not edit below this line
