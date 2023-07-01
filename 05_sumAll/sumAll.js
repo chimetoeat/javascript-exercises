ee317a4b90f5cff29a0f6e754e156cf696e7011e
Vince Ivan Minez <banongofficial28@gmail.com>
2023-07-01 18:03:50 +0800

    Completed Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let min = Math.min(num1,num2);
+    let max = Math.max(num1,num2);
 
+    let sum = 0;
+
+    if (max > 0 && typeof num1 === 'number' && min > 0 && typeof num2 === 'number') {
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+    } else {
+        return "ERROR"
+    }
+        
+
+    return sum;
 };
 
 // Do not edit below this line
